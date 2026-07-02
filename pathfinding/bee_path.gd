e96208ebcfa341a04727d07de5aa56eb0e400267
Mikha <mikharohrs@gmail.com>
2026-07-02 20:33:36 +0100

    [Fixup] Begin work on Bees Algorithm

---
+++ pathfinding/bee_path.gd
@@ -0,0 +1,7 @@
+class_name Beepath
+
+var points: Array[Vector2i]
+var direction_string: Array[String]
+
+func _init() -> void:
+	pass
---
+++ pathfinding/bees_algorithm.gd
@@ -0,0 +1,31 @@
+class_name Beesalgorithm
+
+# Parameters
+var iterations: int
+var scout_bees: int
+var elite_sites: int
+var best_sites: int
+var recruited_bees_for_elite_sites: int
+var recruited_bees_for_remaining_best_sites: int
+var neighbourhood_size: int
+var stagnation_limit: int
+
+# Initialise parameters, the initial population
+func _init() -> void:
+	pass
+
+# The main loop
+func main_loop() -> void:
+	pass
+
+func waggle_dance() -> void:
+	pass
+
+func local_search() -> void:
+	pass
+
+func global_search() -> void:
+	pass
+
+func population_update() -> void:
+	pass
